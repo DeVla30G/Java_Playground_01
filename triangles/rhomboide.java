4719b2f01c3970105f807cc6748ea4e997597264
VladiG <vladigrosjean@gmail.com>
2022-07-13 21:24:35 +0200

    diamonds are the girls best friends

---
+++ triangles/rhomboide.java
@@ -0,0 +1,28 @@
+package triangles;
+
+public class rhomboide {
+    
+    public static void main(String[] args) {
+        int size = 7;
+        int alphas = 65;
+
+        for(int i = 1; i <= size; i++){
+            for(int j = size; j > i; j--){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i*2 -1; k++){
+                System.out.print((char)(alphas+k));
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= size - 1; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (size - i)*2 -1; k++){
+                System.out.print((char)(alphas + k));
+            }
+            System.out.println();
+        }
+    }
+}
